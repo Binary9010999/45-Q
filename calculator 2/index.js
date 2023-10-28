8ca4618e6fb9ca3293012560c57695ca661a6798
Binary9010999 <149241923+Binary9010999@users.noreply.github.com>
2023-10-28 19:03:59 +0500

    Add files via upload
    45 questions ts

---
+++ calculator 2/index.js
@@ -0,0 +1,48 @@
+"use strict";
+// // Question 2
+// Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// // Answer 
+// let usernme= ("Fatima")
+// console.log("Salaam,I am Fatima Muhammad Ali,today i will enlighten u about Typescript")
+//Name Cases: Store a person’s name in a variable, and then print that person’s name
+// in lowercase, uppercase, and titlecase.
+// let nameFormat: string = "Fatima";
+// let toUpperCase = nameFormat.toUpperCase();
+// let toLowerrCase = nameFormat.toLowerCase();
+// let toTitleCase = nameFormat.replace(/\b\w/g, function (char) {return char.toUpperCase(); });
+// console.log(toUpperCase )
+//Question 4
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let usernme = (" Mae West");
+console.log(" You only live once, but if you do it right, once is enough. ");
+// Question 5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// Question6
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+// let book1 = "The cruel prince"
+// let book2 = "The lost sisters"
+// console.log(`best seller books are \n ${book1} \t ${book2}`)
+//Question7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(2 + 2);
+console.log();
+// Question8
+// You should create four lines that look like this:
+// console.log(2+8)
+// console.log(2+8)
+// console.log(2+8)
+// console.log(2+8)
+// question9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+// let favoritenum = 2
+// console.log(`my favourite number is ${favoritenum}`) 
+// question10 
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+//  If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file.
+//   Then write one sentence describing what the program does.
+// let friend = "unknown"
+// //this is my friend
+// console.log(unknown)
+// question 11
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
